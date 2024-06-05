6ddd09f99191334684f39c72d157d946943264e0
je-pa <jieun0502@gmail.com>
2024-06-06 02:12:56 +0900

    ✅ Test: AmericanoTest

---
+++ src/test/java/sample/cafekiosk/unit/beverage/AmericanoTest.java
@@ -0,0 +1,26 @@
+package sample.cafekiosk.unit.beverage;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AmericanoTest {
+  @Test
+  void getName() {
+    Americano americano = new Americano();
+
+    // junit
+    assertEquals(americano.getName(), "아메리카노");
+
+    // assertj
+    assertThat(americano.getName()).isEqualTo("아메리카노");
+  }
+
+  @Test
+  void getPrice() {
+    Americano americano = new Americano();
+
+    assertThat(americano.getPrice()).isEqualTo(4000);
+  }
+}
