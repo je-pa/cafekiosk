e643e80fb77d5497c71359b9bdf24bc47f28aef1
je-pa <jieun0502@gmail.com>
2024-06-07 03:16:58 +0900

    ✨ Features: [CafeKiosk] 주문 생성 시간 제약조건 추가

--- src/main/java/sample/cafekiosk/unit/CafeKioskRunner.java
+++ src/main/java/sample/cafekiosk/unit/CafeKioskRunner.java
@@ -1,8 +1,10 @@
 package sample.cafekiosk.unit;
 
+import java.time.LocalDateTime;
 import lombok.extern.slf4j.Slf4j;
 import sample.cafekiosk.unit.beverage.Americano;
 import sample.cafekiosk.unit.beverage.Latte;
+import sample.cafekiosk.unit.order.Order;
 
 @Slf4j
 public class CafeKioskRunner {
@@ -16,6 +18,8 @@ public class CafeKioskRunner {
 
     int totalPrice = cafeKiosk.calculateTotalPrice();
     log.info("총 주문가격 : {}", totalPrice);
+
+    Order order = cafeKiosk.createOrder(LocalDateTime.now());
   }
 
 }
