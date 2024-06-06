268d1c0b5845dc35257cf23d5d9bf9bc10f54c45
je-pa <jieun0502@gmail.com>
2024-06-07 03:17:43 +0900

    ✅ Test: [CafeKiosk] - 시간 제약조건이 추가된 주문 생성 테스트코드 작성

--- src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
+++ src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -4,10 +4,12 @@ package sample.cafekiosk.unit;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import sample.cafekiosk.unit.beverage.Americano;
 import sample.cafekiosk.unit.beverage.Latte;
+import sample.cafekiosk.unit.order.Order;
 
 class CafeKioskTest {
 
@@ -77,5 +79,43 @@ class CafeKioskTest {
 
     cafeKiosk.clear();
     assertThat(cafeKiosk.getBeverages()).isEmpty();
+  }
+
+  @Test
+  @DisplayName("가게 운영시간에만 성공하는 주문 생성 테스트")
+  void createOrder() {
+    CafeKiosk cafeKiosk = new CafeKiosk();
+    Americano americano = new Americano();
+    cafeKiosk.add(americano);
+
+    Order order = cafeKiosk.createOrder();
+
+    assertThat(order.getBeverages()).hasSize(1);
+    assertThat(order.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+  }
+
+  @Test
+  @DisplayName("주문 생성 테스트 - currentTime을 외부로 뺀 케이스 - 경계값 10시")
+  void createOrderWithCurrentTime() {
+    CafeKiosk cafeKiosk = new CafeKiosk();
+    Americano americano = new Americano();
+    cafeKiosk.add(americano);
+
+    Order order = cafeKiosk.createOrder(LocalDateTime.of(2023, 1, 17, 10, 0));
+
+    assertThat(order.getBeverages()).hasSize(1);
+    assertThat(order.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+  }
+
+  @Test
+  @DisplayName("주문 생성 예외 테스트 - currentTime을 외부로 뺀 케이스 - 경계값 9시")
+  void createOrderOutsideOpenTime() {
+    CafeKiosk cafeKiosk = new CafeKiosk();
+    Americano americano = new Americano();
+    cafeKiosk.add(americano);
+
+    assertThatThrownBy(() -> cafeKiosk.createOrder(LocalDateTime.of(2023, 1, 17, 9, 59)))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("주문 시간이 아닙니다. 관리자에게 문의하세요.");
   }
 }
