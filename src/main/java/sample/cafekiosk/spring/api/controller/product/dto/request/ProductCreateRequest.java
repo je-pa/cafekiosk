0b0f1464eb4d03edd41da07cb7de7212025f9bec
je-pa <jieun0502@gmail.com>
2024-06-19 10:05:53 +0900

    ♻️ Refactor code: [product] 상품생성, 상품조회 리펙토링

--- src/main/java/sample/cafekiosk/spring/api/controller/product/dto/request/ProductCreateRequest.java
+++ src/main/java/sample/cafekiosk/spring/api/controller/product/dto/request/ProductCreateRequest.java
@@ -1,6 +1,10 @@
 package sample.cafekiosk.spring.api.controller.product.dto.request;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 import lombok.Getter;
+import sample.cafekiosk.spring.api.service.product.request.ProductCreateServiceRequest;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.type.ProductSellingStatus;
 import sample.cafekiosk.spring.domain.product.type.ProductType;
@@ -8,9 +12,16 @@ import sample.cafekiosk.spring.domain.product.type.ProductType;
 @Getter
 public class ProductCreateRequest {
 
+  @NotNull(message = "상품 타입은 필수입니다.")
   private ProductType type;
+
+  @NotNull(message = "상품 판매상태는 필수입니다.")
   private ProductSellingStatus sellingStatus;
+
+  @NotBlank(message = "상품 이름은 필수입니다.")
   private String name;
+
+  @Positive(message = "상품 가격은 양수여야 합니다.")
   private int price;
 
   @Builder
@@ -24,6 +35,15 @@ public class ProductCreateRequest {
   public Product toEntity(String nextProductNumber) {
     return Product.builder()
         .productNumber(nextProductNumber)
+        .type(type)
+        .sellingStatus(sellingStatus)
+        .name(name)
+        .price(price)
+        .build();
+  }
+
+  public ProductCreateServiceRequest toServiceRequest() {
+    return ProductCreateServiceRequest.builder()
         .type(type)
         .sellingStatus(sellingStatus)
         .name(name)
