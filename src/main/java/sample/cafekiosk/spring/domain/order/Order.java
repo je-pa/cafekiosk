e8c15df0ba99b426c62c39845d28e7ccfd3d3be2
je-pa <jieun0502@gmail.com>
2024-06-22 01:57:27 +0900

    ✨ Features: [order statistics] 주문 통계

--- src/main/java/sample/cafekiosk/spring/domain/order/Order.java
+++ src/main/java/sample/cafekiosk/spring/domain/order/Order.java
@@ -12,6 +12,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import sample.cafekiosk.spring.domain.BaseEntity;
@@ -44,17 +45,23 @@ public class Order extends BaseEntity {
   @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
   private List<OrderProduct> orderProducts = new ArrayList<>();
 
-  public Order(List<Product> products, LocalDateTime registeredDateTime) {
-    this.orderStatus = INIT;
+  @Builder
+  public Order(List<Product> products, OrderStatus orderStatus,
+      LocalDateTime registeredDateTime) {
     this.totalPrice = calculateTotalPrice(products);
-    this.registeredDateTime = registeredDateTime;
     this.orderProducts = products.stream()
         .map(product -> new OrderProduct(this, product))
         .collect(Collectors.toList());
+    this.orderStatus = orderStatus;
+    this.registeredDateTime = registeredDateTime;
   }
 
   public static Order create(List<Product> products, LocalDateTime registeredDateTime) {
-    return new Order(products, registeredDateTime);
+    return Order.builder()
+        .orderStatus(INIT)
+        .products(products)
+        .registeredDateTime(registeredDateTime)
+        .build();
   }
 
   private int calculateTotalPrice(List<Product> products) {
