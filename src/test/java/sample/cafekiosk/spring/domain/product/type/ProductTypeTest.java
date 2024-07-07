21b86cbb8399e5ac9d7446f1f3e34c9a1bb0b413
je-pa <jieun0502@gmail.com>
2024-07-07 15:59:21 +0900

    ✅ Test: 여러 논리 구조가 들어가 지양해야하는 테스트 코드 예시

--- src/test/java/sample/cafekiosk/spring/domain/product/type/ProductTypeTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/product/type/ProductTypeTest.java
@@ -33,4 +33,30 @@ class ProductTypeTest {
     assertThat(result).isTrue();
   }
 
-}
+  @DisplayName("(여러 논리구조가 들어간) 지양해야하는 방법: 상품 타입이 재고 관련 타입인지를 체크한다.")
+  @Test
+  void containsStockType3() {
+    // given
+    ProductType[] productTypes = ProductType.values();
+
+    for (ProductType productType : productTypes) {
+      if(productType == ProductType.HANDMADE){
+        // when
+        boolean result = ProductType.containsStockType(productType);
+
+        // then
+        assertThat(result).isFalse();
+      }
+      if(productType == ProductType.BAKERY || productType == ProductType.BOTTLE){
+        // when
+        boolean result = ProductType.containsStockType(productType);
+
+        // then
+        assertThat(result).isTrue();
+      }
+
+    }
+  }
+
+
+  }
